5199fa3455cdf161db83d136bc0746ab55f329ff
Adam Gordon <3723289+gordon-to@users.noreply.github.com>
2024-01-18 19:14:10 +0000

    basic server

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "os"
+import {
+	"sys"
+	"os"
+	"net/http"
+}
 
 func main() {
+	done := make(chan int, 1)
+	go func() {
+		signal := make(chan os.Signal, 1)
+		// listen for incoming signals SIGINT and SIGTERM
+		os.Notify(signal, os.Interrupt, sys.SIGTERM)
+		<-signal
+		close(done)
+	}()
+	go httpServer()
+	<-done
 	os.Exit(0)
+}
+
+func httpServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	http.ListenAndServe(":8080", nil)
 }
